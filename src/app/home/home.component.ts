a70046b02c3619b5a9daf636830e25ad9be6eb20
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-11 16:04:08 -0100

    Unsubscribe!
    Unsubscribe!
    Section 13, Lecture 165

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs-compat';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // const myNumbers = Observable.interval(1000);
-    // myNumbers.subscribe(
-    //   (number: Number) => {
-    //     console.log(number);
-    //   }
-    // );
+    const myNumbers = Observable.interval(1000);
+
+    this.numbersObsSubscription = myNumbers.subscribe(
+      (number: Number) => {
+        console.log(number);
+      }
+    );
 
     // const myObservable: Observable<string> = Observable.create((observer: Observer<string>) => {
     //   setTimeout(() => observer.next('First Package'), 2000);
@@ -25,20 +28,25 @@ export class HomeComponent implements OnInit {
     // });
 
     const myObservable: Observable<string> = Observable.create((observer: Observer<string>) => {
-      setTimeout(() => observer.next('First_1 Package'), 1000);
-      setTimeout(() => observer.next('First_2 Package'), 2000);
-      setTimeout(() => observer.next('First_3 Package'), 3000);
+      setTimeout(() => observer.next('First Package'), 2000);
       setTimeout(() => observer.next('Second Package'), 4000);
       // setTimeout(() => observer.error('This does not work'), 5000);
       setTimeout(() => observer.complete(), 5000);
       setTimeout(() => observer.next('Third Package'), 6000);
     });
 
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => console.log(data),
       (error: string) => console.log(error),
       () => console.log('completed')
     );
+  }
+
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
