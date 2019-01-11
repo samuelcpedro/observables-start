139004504232720fbd61c9fbe974bc0c75979c4e
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-11 14:14:13 -0100

    Building & Using a First Simple Observable
    Building & Using a First Simple Observable
    Section 13, Lecture 163

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,12 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000);
+    myNumbers.subscribe(
+      (number: Number) => {
+        console.log(number);
+      }
+    );
   }
 
 }
